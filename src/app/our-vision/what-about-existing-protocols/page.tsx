2e20842c6851e8349b0a6e6a3bc3a4a9ffdc9d28
SamBobBarnes <samuelrbarnes@outlook.com>
2025-09-07 16:21:16 -0500

    wip issues

--- src/app/our-vision/what-about-existing-protocols/page.tsx
+++ src/app/our-vision/what-about-existing-protocols/page.tsx
@@ -2,6 +2,7 @@
 import { PageNavigation } from '@/components/layout/page-navigation';
 import Disclaimer from '@/components/layout/disclaimer';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import LanIcon from '@mui/icons-material/Lan';
 
 export default function ExistingProtocolsPage() {
   const data = [
@@ -58,35 +59,12 @@ export default function ExistingProtocolsPage() {
     { feature: 'Most features are functionly orthogonal.', tcpv4: 'yes', tcpv6: 'yes', terra: 'yes' },
   ];
 
-  /*
-    Textual addresses and names are natively UNICODE.	no	no	yes
-Direction-of-read of textual addresses is:	small-to-large	small-to-large	large-to-small
-Number of bits in digital address:	32	128	64
-Number of bytes in digital address:	4	16	8
-Regular address and current address of a node are disparate.	no	no	yes
-Address resolution (DNS) is integrated into the stack.	no	no	yes
-Multicast is integrated into the stack.	yes	yes	yes
-Topology discovery is integrated into the stack.	no	no	yes
-Route construction is integrated into the stack.	no	no	yes
-Route construction performs large-scale load-balancing.	no	no	yes
-Route construction permits arbitrary packet readdressing.	no	no	yes
-Node maintains single address regardless of number of adapters.	no	no	yes
-Security is integrated into the stack.	no	yes*	yes
-Security typically requires certificates.	yes	yes	no
-Connections are secure by default.	no	no	yes
-Nodes are topologically mobile by default.	no	no	yes
-Connections beteeen applications can be made by port name.	no	no	yes
-Connections between applications can be made by port number.	yes	yes	yes
-API permits out-of-band data.	yes	yes	yes
-API permits transparent auto-re-resolution of connections.	no	no	yes
-API makes gross distinction between reliable and unreliable connections.	yes	yes	no
-Source of packets and target of packets can be different nodes in reliable connections.	no	no	yes
-Connections are resistant to chaotic changes in path MTU.	no	yes	yes
-Rate control is cooperative for nodes along multi-path.	no	no	yes
-Packet header may vary in size.	yes	yes	no
-Most features are functionly orthogonal.	yes	yes	yes
-
-   */
+  const getIcon = (text: string): Element => {
+    if (text === 'yes') {
+      return <LanIcon />;
+    }
+    return <span>{text}</span>;
+  };
 
   return (
     <div className="m-4 vision flex justify-center">
@@ -112,18 +90,35 @@ Most features are functionly orthogonal.	yes	yes	yes
               <TableHeader>
                 <TableRow>
                   <TableHead className="w-[100px]">Feature</TableHead>
-                  <TableHead>TCP/IP4</TableHead>
-                  <TableHead>TCP/IP6</TableHead>
-                  <TableHead>TerraStack</TableHead>
+                  <TableHead
+                    style={{ color: 'var(--primary-hover)' }}
+                    className={'text-center'}
+                  >
+                    TCP/IP4
+                  </TableHead>
+                  <TableHead
+                    style={{ color: 'var(--primary-hover)' }}
+                    className={'text-center'}
+                  >
+                    TCP/IP6
+                  </TableHead>
+                  <TableHead
+                    style={{ color: 'var(--primary-hover)' }}
+                    className={'text-center'}
+                  >
+                    TerraStack
+                  </TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
                 {data.map((row) => (
                   <TableRow key={row.feature}>
-                    <TableCell className="font-medium overflow-auto whitespace-normal">{row.feature}</TableCell>
-                    <TableCell>{row.tcpv4}</TableCell>
-                    <TableCell>{row.tcpv6}</TableCell>
-                    <TableCell>{row.terra}</TableCell>
+                    <TableCell className="font-medium overflow-auto whitespace-normal w-[100%]">
+                      {row.feature}
+                    </TableCell>
+                    <TableCell className={'text-center'}>{getIcon(row.tcpv4)}</TableCell>
+                    <TableCell className={'text-center'}>{row.tcpv6}</TableCell>
+                    <TableCell className={'text-center'}>{row.terra}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
