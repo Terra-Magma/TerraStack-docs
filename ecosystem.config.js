53cf3c6606d6197b258fc5ca2496793e488dab70
SamBobBarnes <samuelrbarnes@outlook.com>
2025-09-22 12:55:57 -0500

    fix config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,7 @@ module.exports = {
       watch: ['.'],
       autorestart: true,
       ignore_watch: ['node_modules', 'out'],
-      pre_reload: 'npm install && npm run build',
-      post_update: 'npm install && npm run build',
+      post_update: ['npm install', 'npm run build'],
       env: {
         PM2_SERVE_PATH: '.',
         PM2_SERVE_PORT: 3000,
