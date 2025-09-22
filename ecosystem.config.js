f4a26f17503b703502c6961700c234da013b71c9
SamBobBarnes <samuelrbarnes@outlook.com>
2025-09-22 10:24:28 -0500

    update to deployment pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,10 @@ module.exports = {
 
   deploy: {
     production: {
-      user: 'SSH_USERNAME',
-      host: 'SSH_HOSTMACHINE',
+      host: 'localhost',
       ref: 'origin/main',
       repo: 'git@github.com:terra-master/terrastack-docs.git',
-      path: 'C:/inetpub/TerraMagma',
+      path: '/Users/sbarnes/Code/terrastack-test',
       'pre-deploy-local': '',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
