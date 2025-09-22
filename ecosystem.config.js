a697dfda4fbbabbddcf106d0449dbc5583677540
SamBobBarnes <samuelrbarnes@outlook.com>
2025-09-22 15:56:20 -0500

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,7 @@ module.exports = {
     {
       name: 'terrastack-docs',
       script: 'serve',
-      watch: '.',
       autorestart: true,
-      ignore_watch: ['node_modules', 'out'],
-      post_update: ['npm install', 'npm run build'],
       env: {
         PM2_SERVE_PATH: './out',
         PM2_SERVE_PORT: 3000,
