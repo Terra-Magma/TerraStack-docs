23f04adb0b9eba9e2e9e5ae5223aa3fc8588f12b
SamBobBarnes <samuelrbarnes@outlook.com>
2025-09-22 10:26:05 -0500

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
     production: {
       host: 'localhost',
       ref: 'origin/main',
-      repo: 'git@github.com:Terra-Magma/TerraStack-docs.git',
+      repo: 'git@github.com:Terra-Magma/TerraStack-docs',
       path: '/Users/sbarnes/Code/terrastack-test',
       'pre-deploy-local': '',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
