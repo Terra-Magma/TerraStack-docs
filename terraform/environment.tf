ff15435408851b3c9df59f9fb418d78bc8dd4267
SamBobBarnes <sbarnes@vermeer.com>
2025-10-28 14:53:40 -0500

    update terraform

---
+++ terraform/environment.tf
@@ -0,0 +1,5 @@
+﻿resource "portainer_environment" "terramagma-web-host" {
+  name                = "Terramagma Web Host"
+  environment_address = "${var.terramagma_local_ip}:9001"
+  type                = 2
+}
