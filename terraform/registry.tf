5be4649ec76bab9fc27b22db75e4aa7dbf0b5f6e
SamBobBarnes <sbarnes@vermeer.com>
2025-10-28 14:37:02 -0500

    update registry type

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,10 +1,8 @@
 resource "portainer_registry" "github" {
-  name                     = "GitHub"
-  type                     = 8
-  url                      = "ghcr.io"
-  authentication           = true
-  username                 = var.github_registry_username
-  password                 = var.github_registry_access_token
-  github_use_organisation  = true
-  github_organisation_name = "Terra-Magma"
+  name           = "GitHub"
+  type           = 3
+  url            = "ghcr.io"
+  authentication = true
+  username       = var.github_registry_username
+  password       = var.github_registry_access_token
 }
