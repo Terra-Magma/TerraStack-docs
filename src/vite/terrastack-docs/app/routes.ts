e4c0988c4deea84dc9e5213e92acbb81334bb25a
SamBobBarnes <samuelrbarnes@outlook.com>
2025-11-27 13:04:18 -0500

    wip

--- src/vite/terrastack-docs/app/routes.ts
+++ src/vite/terrastack-docs/app/routes.ts
@@ -1,7 +1,7 @@
-import { index, route, type RouteConfig } from '@react-router/dev/routes';
+import { index, layout, route, type RouteConfig } from '@react-router/dev/routes';
 
 export default [
-  route('/', 'routes/_layout.tsx', [
+  layout('routes/_layout.tsx', [
     index('routes/home.tsx'),
     route('terrastack', 'routes/terrastack.tsx'),
     route('*', 'routes/not-found.tsx'),
