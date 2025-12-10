4c0c8c7fe3a22d938cc06a8627a389d15f6dc799
SamBobBarnes <samuelrbarnes@outlook.com>
2025-12-09 19:11:39 -0600

    add webhook

--- terraform/stack.tf
+++ terraform/stack.tf
@@ -5,6 +5,7 @@ resource "portainer_stack" "terrastack-docs" {
   endpoint_id     = portainer_environment.terramagma-web-host.id
   pull_image      = true
   stack_file_path = "${path.module}/../docker-compose.yml"
+  stack_webhook   = true
 }
 
 resource "portainer_stack_webhook" "trigger_my_stack" {
